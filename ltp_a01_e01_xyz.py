8b1b54f28f955a47d16dd41dc0e74e6a560ac5e7
traceform <rafael.contato71@gmail.com>
2025-08-26 08:46:51 -0300

    Update ltp_a01_e01_xyz.py

--- ltp_a01_e01_xyz.py
+++ ltp_a01_e01_xyz.py
@@ -1,5 +1,8 @@
-# Escreva um algoritmo para armazenar o valor 20 em uma variável X e o valor 5 em uma variável Y. A seguir, armazenar a soma do valor de X com o de Y em uma variável Z. Escrever (na tela) o valor armazenado em X, em Y e em Z
-
+'''
+Escreva um algoritmo para armazenar o valor 20 em uma variável X e o valor 5 em uma variável Y.
+A seguir, armazenar a soma do valor de X com o de Y em uma variável Z.
+Escrever (na tela) o valor armazenado em X, em Y e em Z
+'''
 x, y = 20, 5
 z = x + y
 print(f"x = {x}, y = {y}, z = {z}")
