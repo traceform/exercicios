152d1cd4059e6762aefb73400330a23a013c7c3a
traceform <rafael.contato71@gmail.com>
2025-08-26 08:49:10 -0300

    Update meses.py

--- meses.py
+++ meses.py
@@ -1,3 +1,6 @@
+'''
+Escrever um código python que baseado no valor da variável mês do tipo numérico seja impresso o mês de modo extenso.
+'''
 meses = {1:'Janeiro',2:'Fevereiro',3:'Março',4:'Abril',5:'Maio',6:'Junho',7:'Julho',8:'Agosto',9:'Setembro',10:'Outubro',11:'Novembro',12:'Dezembro'}
 
 invalido = True                                                                       # Criação da variável para controle da repetição do while
@@ -11,3 +14,4 @@ while invalido:
             raise ValueError                                                          # Se o número não estiver entre 1 e 12 e/ou não for inteiro, chama um erro para alertar o usuário do que causou o erro
     except ValueError:
         print("ERRO: Número inválido! Só são aceitos números inteiros de 1 a 12.")    # Avisa ao usuário que há um erro e repete o while do início, também ocorre quando algum caractere não-numérico é digitado no input (como letras) ao invés de quebrar o código abruptamente, validando o input.
+
