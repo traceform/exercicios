310a53b18c06c668421235bb3e1fdb6e258ba13a
traceform <rafael.contato71@gmail.com>
2025-08-26 09:56:07 -0300

    Create ltp_a01_e02_qtd_vogais_consoantes.py

---
+++ ltp_a01_e02_qtd_vogais_consoantes.py
@@ -0,0 +1,18 @@
+# Escrever um código python que baseado no valor da variável que armazene um texto, quantifique a quantidade de vogais e consoantes.
+vogais = ['a', 'e', 'i', 'o', 'u']
+vogaisAcentuadas = ['á', 'é', 'í', 'ó', 'ú', 'à', 'â', 'ê', 'ô', 'ã', 'õ', 'ü']
+consoantes = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+
+texto = "Você está no meu coração!"
+qtdVogais = 0
+qtdConsoantes = 0
+
+for l in texto.lower():
+    if l in vogais or l in vogaisAcentuadas:
+        qtdVogais += 1
+    elif l in consoantes or l == 'ç':
+        qtdConsoantes += 1
+print(f"Texto: {texto}")
+print(f"Quantidade total de caracteres: {len(texto)}")
+print(f"Quantidade de vogais: {qtdVogais}")
+print(f"Quantidade de consoantes: {qtdConsoantes}")
