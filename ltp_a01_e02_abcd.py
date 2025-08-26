3c0d7b27be0475c5578a2e7c7254814705ea3e35
traceform <rafael.contato71@gmail.com>
2025-08-26 08:46:15 -0300

    Create ltp_a01_e02_abcd.py

---
+++ ltp_a01_e02_abcd.py
@@ -0,0 +1,8 @@
+'''
+Escreva um algoritmo para armazenar o valor 4 em uma variável A e o valor 3 em uma variável B.
+A seguir, armazenar a soma de A com B em uma variável C e a subtração de A com B em uma variável D.
+Escrever o valor de A, B, C e D e também escrever a mensagem “Fim do Algoritmo”.
+'''
+a, b = 4, 3
+c, d = a + b, a - b
+print(f"a = {a}, b = {b}, c = {c}, d = {d}\nFim do Algoritmo")
