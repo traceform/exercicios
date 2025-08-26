e45a5b1c2e8f9047b1224cfc94512ebd9792be60
traceform <rafael.contato71@gmail.com>
2025-08-26 13:52:40 -0300

    Update ltp_a03_e03_qtd_palavras.py

--- ltp_a03_e03_qtd_palavras.py
+++ ltp_a03_e03_qtd_palavras.py
@@ -1,4 +1,4 @@
 # Escrever um código python que armazene um texto e, baseado no valor da variável, quantifique a quantidade de palavras.
-texto = "senac é legal".upper().strip().split()
-print(texto)
+texto = "senac é legal".strip().split()
+#print(texto)
 print(len(texto))
