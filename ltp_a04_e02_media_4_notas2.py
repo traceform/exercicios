b62bb0c70b6b73310cd8774de2c74cbaee5d6fc3
traceform <rafael.contato71@gmail.com>
2025-08-28 11:45:40 -0300

    Add files via upload

--- ltp_a04_e02_media_4_notas2.py
+++ ltp_a04_e02_media_4_notas2.py
@@ -1,17 +1,23 @@
 def entradaDados(qtdNotas):
+    ''' int -> lista
+    Recebe a quantidade de notas, cada nota e devolve uma lista de notas
+    '''
     notas = []
     for n in range(1, qtdNotas+1, 1):
         invalido = True
         while invalido:
             try:
                 nota = float(input(f"Informe a sua nota {n}: "))
-                notas.append(nota)
-                invalido = False
+                notas.append(nota) # Adiciona a nota à lista
+                invalido = False # Termina o loop
             except:
                 print("ERRO: Número inválido!")
     return notas
 
 def calculaMedia(notas):
+    ''' lista -> float
+    Soma os valores de uma lista e calcula a sua média
+    '''
     soma = 0
     for n in notas:
         soma += n
