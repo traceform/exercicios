87d14d277f5e880f43729484fb7a1b336e0d82e6
traceform <rafael.contato71@gmail.com>
2025-09-13 11:22:30 -0300

    Update and rename financeiro2.py to ltp_a06_e02_juros.py

--- ltp_a06_e02_juros.py
+++ ltp_a06_e02_juros.py
@@ -1,6 +1,8 @@
-from util import calculaJuros
+#from util import calculaJuros
+from ltp_a06_e01_util import calculaJuros
 
 valor = float(input("Informe o valor do produto: R$"))
 juros = float(input("Informe o valor dos juros simples: %"))
 
 print(f"Valor: R${calculaJuros(valor, juros):.2f}")
+
