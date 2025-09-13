df172c3a0340da6c53a3af749f143b6ff91faf94
traceform <rafael.contato71@gmail.com>
2025-09-13 11:20:02 -0300

    Update and rename util.py to ltp_a06_e01_util.py

--- ltp_a06_e01_util.py
+++ ltp_a06_e01_util.py
@@ -1,5 +1,8 @@
 def calculaDesconto(valor: float, desconto: float) -> float:
+    """Retorna um valor flutuante de um produto com o desconto já aplicado"""
     return valor - (valor * desconto / 100)
 
 def calculaJuros(valor, juros):
+    """Retorna o valor flutuante de um item com os juros simples já aplicados"""
     return valor * (1 + (juros / 100))
+
