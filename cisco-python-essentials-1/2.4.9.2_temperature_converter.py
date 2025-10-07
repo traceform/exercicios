818bc467096f675a6e03ffc1d110cfc8f39118a5
traceform <rafael.contato71@gmail.com>
2025-10-07 17:10:07 -0300

    Create 2.4.9.2_temperature_converter.py

---
+++ cisco-python-essentials-1/2.4.9.2_temperature_converter.py
@@ -0,0 +1,8 @@
+c = 25
+f = 77
+
+celsius_to_fahrenheit = (c * 9/5) + 32
+fahrenheit_to_celsius = (f - 32) * 5/9
+
+print(c, "celsius is", round(celsius_to_fahrenheit, 2), "fahrenheit")
+print(f, "fahrenheit is", round(fahrenheit_to_celsius, 2), "celsius")
