9f61747c92f3f8e638c6d93d51fd4eefc6738fd1
traceform <rafael.contato71@gmail.com>
2025-10-07 17:01:49 -0300

    Create 2.4.9_dollars_euros_converter.py

---
+++ cisco-python-essentials-1/2.4.9_dollars_euros_converter.py
@@ -0,0 +1,8 @@
+dollars = 100
+euros = 100
+
+dollars_to_euros = dollars * 0.86
+euros_to_dollars = euros / 0.86
+
+print(dollars, "dollars is", round(dollars_to_euros, 2), "euros")
+print(euros, "euros is", round(euros_to_dollars, 2), "dollars")
