efc4aedf72f540e5cbc176f36374d3cf9fe7b373
traceform <rafael.contato71@gmail.com>
2025-10-16 11:40:13 -0300

    Update ltp_a10_projeto.py

--- ltp_a10_projeto.py
+++ ltp_a10_projeto.py
@@ -26,3 +26,36 @@ print(f"{15 * '='} INFO NO DUPLICATES {15 * '='}\n", dados.info())
 # Q2 - 50% - Quartilha 2 - Média
 # Q1 - 25% - Quartilha 1
 # LI - Limite Inferior
+
+
+
+'''
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dados = pd.read_csv('Titanic-Dataset.csv', sep=',')
+
+print(f"{15 * '='} DADOS {15 * '='}\n", dados)
+print(f"{15 * '='} HEAD {15 * '='}\n", dados.head())
+
+print(f"{15 * '='} INFO {15 * '='}\n", dados.info())
+print(f"{15 * '='} DESCRIBE {15 * '='}\n", dados.describe())
+dados = dados.drop_duplicates()
+print(f"{15 * '='} INFO NO DUPLICATES {15 * '='}\n", dados.info())
+
+dados['Age'].hist()
+plt.show()
+
+# PLN: NLTK, SPACY
+# IMG: OPENCV
+# Oracle lançou produto cujo slogan é que ter um DBA não vai ser mais necessário.
+# DBAs estão indo para análise de dados, tem muito emprego nessa área
+# Data Science Academy
+# std = desvio padrão, é mais confiável que a média
+# Remover outliers dos dados para não gerar análises errôneas
+# LS - Limite Superior
+# Q3 - 75% - Quartilha 3
+# Q2 - 50% - Quartilha 2 - Média
+# Q1 - 25% - Quartilha 1
+# LI - Limite Inferior
+'''
