eaefbb655e9340b74bf2b8a923b292c7250bbd98
traceform <rafael.contato71@gmail.com>
2025-10-16 10:39:47 -0300

    Update and rename projeto.py to ltp_a10_projeto.py

--- ltp_a10_projeto.py
+++ ltp_a10_projeto.py
@@ -21,3 +21,8 @@ print(f"{15 * '='} INFO NO DUPLICATES {15 * '='}\n", dados.info())
 # Data Science Academy
 # std = desvio padrão, é mais confiável que a média
 # Remover outliers dos dados para não gerar análises errôneas
+# LS - Limite Superior
+# Q3 - 75% - Quartilha 3
+# Q2 - 50% - Quartilha 2 - Média
+# Q1 - 25% - Quartilha 1
+# LI - Limite Inferior
