7314f35b4b5cda7a282588a7dcda230706aa0c86
traceform <rafael.contato71@gmail.com>
2025-10-16 23:42:16 -0300

    Update 2.6.10_expression.py

--- cisco-python-essentials-1/2.6.10_expression.py
+++ cisco-python-essentials-1/2.6.10_expression.py
@@ -56,19 +56,32 @@ y = -0.19258202567760344
 '''
 
 def test_code():
-    input_list = [1, 10, 100, -5]
-    output_list = [0.6000000000000001, 0.09901951266867294, 0.009999000199950014, -0.19258202567760344]
+    input_list = [1, 10, 100, -5, 0, 'a', True, False]
+    output_list = [0.6000000000000001, 0.09901951266867294, 0.009999000199950014, -0.19258202567760344, None, None, 0.6000000000000001, None]
     for i in range(0,len(input_list)):
         input = input_list[i]
         output = output_list[i]
-        if math_expression(input) == output:
+        result, msg = math_expression(input)
+        if result == output:
             print(f"Success! {input} returns {output}")
         else:
             print(f"Error! {input} does NOT result in {output}")
 
 def math_expression(x):
-    y = 1/(x + 1/(x + 1/(x + 1/x)))
-    return y
+    y, msg = None, None
+    try:
+        y = 1/(x + 1/(x + 1/(x + 1/x)))
+    except ZeroDivisionError:
+        msg = "Error! Division by zero is not allowed!"
+    except:
+        msg = "Something went wrong!"
+    return y, msg
 
 #test_code()
-print("y =", math_expression(float(input("Enter value for x: "))))
+
+if __name__ == "__main__":
+    y, msg = math_expression(float(input("Enter value for x: ")))
+    if y is None:
+        print(msg)
+    else:
+        print("y =", y)
