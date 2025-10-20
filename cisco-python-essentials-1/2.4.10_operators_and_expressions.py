72d827c7441a04d34c9b6b75a8cf8a9e1391c29a
traceform <rafael.contato71@gmail.com>
2025-10-20 19:24:13 -0300

    new

--- cisco-python-essentials-1/2.4.10_operators_and_expressions.py
+++ cisco-python-essentials-1/2.4.10_operators_and_expressions.py
@@ -2,7 +2,7 @@
 
 ## Scenario
 
-Take a look at the code in the editor: it reads a `float` value, puts it into a variable named `x`, and prints the value of a variable named `y`. Your task is to complete the code in order to evaluate the following expression:
+Take a look at the code in the editor: it reads a `float` value, puts it into a variable named `x`, and prints the value of a variable named `y`. Your task is to complete the code in order to evaluate the following expression:
 
 3x³- 2x² + 3x - 1
 
