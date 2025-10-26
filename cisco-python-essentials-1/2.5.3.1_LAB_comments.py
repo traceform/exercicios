61b2f1eeca3851b021ee27218358fc7f354c5c2d
traceform <rafael.contato71@gmail.com>
2025-10-26 14:14:17 -0300

    new

---
+++ cisco-python-essentials-1/2.5.3.1_LAB_comments.py
@@ -0,0 +1,9 @@
+# this program computes the number of seconds in a given number of hours
+
+hours = 2
+seconds = 3600 # number of seconds in 1 hour
+
+print("Hours:", hours)
+print("Seconds in hours:", hours * seconds)
+
+print('Goodbye')
