d7323222fbfcf11e7d2ae35870380e90a14300f7
traceform <rafael.contato71@gmail.com>
2025-10-26 18:09:54 -0300

    new

--- cisco-python-essentials-1/3.2.14.1_LAB_block_pyramid.py
+++ cisco-python-essentials-1/3.2.14.1_LAB_block_pyramid.py
@@ -6,20 +6,20 @@ blocks = int(input("Enter the amount of blocks you have: "))
 if blocks <= smallest_pyramid:
     print(f"It is not possible to build a pyramid with {blocks} blocks!")    
 else:
-    layer, total_layers = 1, 0
+    height, needed_blocks  = 0, 1
     
     # Check if the pyramid can be built by comparing the remaining blocks
     # to the block amount needed to complete the next layer
-    can_be_built = blocks >= layer
+    can_be_built = blocks >= needed_blocks
 
     while can_be_built:
         # Add a layer to the pyramid
-        total_layers += 1
+        height += 1
         # Remove previously used blocks from the block amount
-        blocks -= layer  
+        blocks -= needed_blocks
         # Calculate the next layer's needed block amount
-        layer += 1
+        needed_blocks += 1
         # Check if there are enough blocks to build the next layer
-        can_be_built = blocks >= layer
+        can_be_built = blocks >= needed_blocks
 
-print(f"The height of the pyramid: {total_layers}")
+print(f"The height of the pyramid: {height}")
