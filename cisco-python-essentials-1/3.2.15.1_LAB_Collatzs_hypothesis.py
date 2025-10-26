719970485d61c68c5b8e64015e78a74b1c48b67d
traceform <rafael.contato71@gmail.com>
2025-10-26 19:03:34 -0300

    new

---
+++ cisco-python-essentials-1/3.2.15.1_LAB_Collatzs_hypothesis.py
@@ -0,0 +1,24 @@
+# Version 1
+steps = 0
+
+try:
+    # Step 1: take any non-negative and non-zero integer number and name it c0
+    c0 = int(input("Type any non-negative non-zero integer number: "))
+    if c0 < 0:
+        raise ValueError
+except ValueError:
+    print("Only non-negative non-zero integer numbers allowed!")
+
+# Step 4: if c0 ≠ 1, go back to point 2
+while c0 != 1:
+    # Step 2: if it's even, evaluate a new c0 as c0 ÷ 2
+    if c0 % 2 == 0:
+        c0 //= 2
+    # Step 3: otherwise, if it's odd, evaluate a new c0 as 3 × c0 + 1
+    else:
+        c0 = 3 * c0 + 1
+
+    print(c0)
+    steps += 1
+
+print(f"steps = {steps}")
